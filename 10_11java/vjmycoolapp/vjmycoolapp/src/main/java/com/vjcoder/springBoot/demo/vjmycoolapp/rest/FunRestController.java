bc8c496eae0d40c436df73861be1468485d6b5dc
Vijay <SharmaVijendra2412@gmail.com>
2024-08-08 11:41:44 +0530

    done

--- 10_11java/vjmycoolapp/vjmycoolapp/src/main/java/com/vjcoder/springBoot/demo/vjmycoolapp/rest/FunRestController.java
+++ 10_11java/vjmycoolapp/vjmycoolapp/src/main/java/com/vjcoder/springBoot/demo/vjmycoolapp/rest/FunRestController.java
@@ -1,2 +1,26 @@
-package com.vjcoder.springBoot.demo.vjmycoolapp.rest;public class FunRestController {
+package com.vjcoder.springBoot.demo.vjmycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    //expose "/" that return "Hello world"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Wolrd";
+    }
+
+    //expose a new endpoint for "Workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Today is chest exercise Day";
+    }
+
+    // expose a new exndpoint for "diet"
+    @GetMapping("/diet")
+    public String getDailyDiet(){
+        return "dies is good for health and Muscle";
+    }
+
 }
--- java11-12/language.java
+++ java11-12/language.java
@@ -1701,6 +1701,59 @@ class vj {
         // }
 
         // ------Multiple catch block---------
+        // try {
+        //     int a[] = new int[5];
+        //     a[5] = 30/2;
+        // } catch (ArithmeticException e) {
+        //     System.out.println("Arithmetic Exception occur");
+        // }
+        // catch(ArrayIndexOutOfBoundsException e){
+        //     System.out.println("ArrayIndexOutOfBoundsException");
+        // }
+        // catch(Exception e){
+        //     System.out.println("rest of code");
+        // }
+
+
+
+        // try {
+        //     String s = null;
+        //     System.out.println(s.length());
+
+        // } catch (ArithmeticException e) {
+        //      System.out.println("Arithmetic Exception occur");
+        // }
+        // catch(ArrayIndexOutOfBoundsException e){
+        //     System.out.println("ArrayIndexOutOfBoundsException");
+        // }
+        // catch(Exception e){
+        //     System.out.println("rest of code");
+        // }
+
+        try {
+
+            try {
+                System.out.println("divide by 0");
+                int a = 10/0;
+                System.out.println(a);
+            } catch (ArithmeticException e) {
+                
+                System.out.println(e);
+            }
+
+            try {
+                int a[] = new int[5];
+                a[5]= 4;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+            }
+        } catch (Exception e) {
+           System.out.println("handled");
+        }
+// System.out.println("abc");
+finally{
+    System.out.println("abcd");
+}
 
     }
 }
